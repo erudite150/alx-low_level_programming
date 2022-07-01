aa754f1f96fcbb830b4eddaa49d94653214a6dd8
erudite150 <erudite150@gmail.com>
2022-07-01 01:50:07 -0700

    4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main(void)
+/**
+*main -Entry point
+*Return: Always 0 (success)
+*/
+int main()
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+	puts("\"programming is like a building a multilingual puzzle"):
+		return(0)
 }
