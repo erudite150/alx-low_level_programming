db02bcf844aee3bd549d3a76b982da1254e257f7
erudite150 <erudite150@gmail.com>
2022-07-08 16:54:24 -0700

    -main..c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,14 +11,16 @@ int main(void)
 
 	printf("infinte loop incoming :(\n")
 
-		i = 0;
-	/**
-	 * while (i < 10)
-	 * {
-	 * putchar(i);
-	 * }
-	 */
+	i = 0;
+
+	 /*
+	  * while (i < 10)
+{
+	putchar(i);
+}
+	  */
+
 	printf("infinite loop avoided \\o/\n");
 
 	return (0);
-}
+i
