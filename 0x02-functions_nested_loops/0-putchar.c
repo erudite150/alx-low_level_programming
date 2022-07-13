f07479a5d194e3e5c593459ab6407516c9cc50c5
erudite150 <erudite150@gmail.com>
2022-07-13 03:40:36 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * main - Prints putchar
+ * main - Prints "_putchar"
+ *
  * Return: always 0
  */
-int main(void)
+
+int mainn(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char ch[8] = "_putchar";
+	int i;
+
+	for (i = 0; i<8; i++)
+		_putchar(ch[i]);
 	_putchar('\n');
 
 	return (0)
