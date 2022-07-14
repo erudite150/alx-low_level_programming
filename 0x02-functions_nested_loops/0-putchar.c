32a495c731bbb840d25b82beb233a55b2064781a
erudite150 <erudite150@gmail.com>
2022-07-13 17:06:29 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  * Return: always 0
  */
 
-int mainn(void)
+int main(void)
 {
 	char ch[8] = "_putchar";
 	int i;
 
-	for (i = 0; i<8; i++)
-		_putchar(ch[i]);
-	_putchar('\n');
+	for (i = 0; i < 8; i++)
+			_putchar(ch[i]);
+	_putchar('\n')
 
-	return (0)
+		return (0);
 }
